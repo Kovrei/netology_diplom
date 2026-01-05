ee34b15ea2e4df6afb7039fd5878d1d41481add5
reikov <rei169kov@yandex.ru>
2026-01-05 14:24:45 +0500

    test4

--- src/terraform/provider.tf
+++ src/terraform/provider.tf
@@ -10,9 +10,9 @@ terraform {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
-    bucket     = "diplom-osipenkov"
-    region     = "ru-central1-a"
-    key        = "terraform.tfstate"
+    #bucket     = "diplom-osipenkov"
+    #region     = "ru-central1-a"
+    #key        = "terraform.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
